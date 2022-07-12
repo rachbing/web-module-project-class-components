6ea213d7a76aae1e5a9ba27b4fcc216d7e5c1aa9
Rachel Bingham <rachelbingham@Rachels-MacBook-Air.local>
2022-07-11 18:50:34 -0600

    good studying and practicing

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,72 @@
 import React from 'react'
 
+import TodoList from './TodoList';
+import Form from './Form';
+
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          name: "walk doggies",
+          id: Date.now(),
+          completed: false
+        }
+      ]
+    }
+  }
+
+  handleAdd = (name) => {
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo => {
+        if(todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+
+        return todo;
+      })
+    })
+  }
+
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
-        Todo App
+
+        <TodoList handleToggle={this.handleToggle} todos={todos}/>
+        <Form handleAdd={this.handleAdd}/>
+        <button onClick={this.handleClear}>Clear</button>
       </div>
     )
   }
+
+
 }
